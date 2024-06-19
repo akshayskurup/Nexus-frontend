d39cf14d7306b1ba16e68611ec3dfaa192884fae
Akshay <akshayskurup@gmail.com>
2024-06-19 13:23:52 +0530

    added dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,23 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
+  },
+  optimizeDeps: {
+    exclude: ['@zegocloud/zego-uikit-prebuilt']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'zego-uikit-prebuilt': ['@zegocloud/zego-uikit-prebuilt']
+        }
+      }
+    }
   }
 })
